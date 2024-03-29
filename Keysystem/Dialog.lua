8d487dac0636e1ad37d06494295f2047ee33bf07
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-29 10:48:44 +0500

    Create Dialog.lua

---
+++ Keysystem/Dialog.lua
@@ -0,0 +1,11 @@
+local btn1,btn2 = "OK","Get Key"
+
+local CustomDialog = require("CustomDialog")
+CustomDialog:createDialog()
+CustomDialog:setTitleText( -1347(-43765- 78456))
+CustomDailog:setMsgText(-846( 473- 336(473[3478-3(43-346,"/239/",-237[437])])))
+CustomDialog:button1(btn1)
+CustomDialog:button2(btn2)
+
+--Key system function
+return(functiom(ll1l1l1l1l11l1l)local l1l1l1l1l1l1l1l1l1l1l1l1l12={"D"?E?"C?"E?"C?"E?"?C"FR?"C?R?"C?"R?"F"R?"C?"R?C"F?"R?CF??R?C?RFC/"}end
