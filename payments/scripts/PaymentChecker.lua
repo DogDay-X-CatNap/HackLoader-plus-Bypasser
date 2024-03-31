ddd6b3350504d5fec2a44338f83b16f3c6db0699
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-31 17:37:23 +0500

    Create PaymentChecker.lua

---
+++ payments/scripts/PaymentChecker.lua
@@ -0,0 +1,12 @@
+local function ch()
+  return(function(...)return(.."setmetatable"..)local a = setmetatable;local E={"replace.tab","redirect('b','uy','coffe','.com','kylemv','fxckingkylemv'","end","io"},decall()g.unpack()table.unpack();end;end(ooo,Ooo,oOo,ooO,...)
+ch()
+local function cs()
+    CustomDialog:createDialog()
+    CustomDialog:setInfoText()
+    cs()
+usedPay = {}
+notUsedPay = {}
+local PayMent = {"\98\117\121\109\101\97\99\111\102\102\101\101\46\99\111\109\47\102\120\99\107\105\110\103\107\121\108\101\109\118\10"}
+if PayMent == usedPay.web then print(cs("You have successfully bought premium"))
+    if PayMent == notUsedPay.web then
